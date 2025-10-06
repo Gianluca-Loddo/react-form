0ffc1723eeff95f702da78eb6dbc235f6436241a
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-10-07 00:44:17 +0200

    installed Boostrap from terminal ad import css

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import './App.css'
 function App() {
  
   return (
-    <>
-     
+    <> {/* React Fragment */}
+     <h1>react-form</h1>
     </>
-  )
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'  // import del CSS di Bootstrap
+import './App.jsx' // import del file App.jsx
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
