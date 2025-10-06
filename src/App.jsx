6d0cfaf134c0b94cd8487749872964f6bd517cea
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-10-07 01:08:17 +0200

    Milestone 1 completed: used metod map & key

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react'
+import { useState } from 'react' // import di useState (hook di React)
 
 import './App.css'
 
 function App() {
- 
+
+  const [articles] = useState([ 
+    /* stato iniziale con array di oggetti, ognuno con id e title, senza la funzione di setArticles perchè non serve
+      const [articles, setArticles] = useState([...]);*/ 
+    
+    { id: 1, title: "Primo post di prova" },
+    { id: 2, title: "Secondo post di prova" },
+    { id: 3, title: "Terzo post di prova" },
+  ]);
+
+
+
   return (
     <> {/* React Fragment */}
-     <h1 className='text-center mb-4'>react-form</h1>
+      <div className="container py-4"> {/* Bootstrap container con padding verticale */}
+        <h1 className="text-center mb-4">react-form</h1> {/* Titolo centrato con margine inferiore */}
+
+        <h2 className="mb-3">Articoli</h2>
+
+        {/* Milestone 1 - lista titoli */}
+        <ul className="list-group"> {/* Lista di Bootstrap */}
+          {articles.map((article, i) => (
+            <li key={i} className="list-group-item"> {/* Elemento di lista di Bootstrap */}
+              {article.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </>
   );
 }
 
 export default App
+
+
+/*  Milestone 1
+
+
+Appunti sull'uso della key in React:
+
+ | Sintassi        | Cosa fa                              | Quando usarla            |
+| --------------- | ------------------------------------ | ------------------------ |
+| `key={a.id}`    | Usa l’id dell’oggetto (dot notation) | Migliore e più stabile |
+| `key={index}`   | Usa l’indice del ciclo               | Ok per liste statiche |
+| `key={a.title}` | Usa un altro campo univoco           | Se non si ha un id      |
+
+*/
