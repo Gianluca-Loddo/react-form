49fa8e2d74e46151439fe16f647e3ab78e836407
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-10-07 17:05:52 +0200

    changed articles: from object array to strin array

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,55 @@ import { useState } from 'react' // import di useState (hook di React)
 import './App.css'
 
 function App() {
-
+  // stato iniziale con array di oggetti (articoli)
   const [articles, setArticles] = useState([
-    { id: 1, title: "Primo post di prova" },
-    { id: 2, title: "Secondo post di prova" },
-    { id: 3, title: "Terzo post di prova" },
+    "Primo post di prova",
+    "Secondo post di prova"
+    "Terzo post di prova",
   ]);
 
+  // stato per L'INPUT del nuovo articolo
   const [newTitle, setNewTitle] = useState(''); // stato per il titolo del nuovo articolo
 
-  return (
-    <> {/* React Fragment */}
-      <div className="container py-4"> {/* Bootstrap container con padding verticale */}
-        <h1 className="text-center mb-4">react-form</h1> {/* Titolo centrato con margine inferiore */}
 
-        <h2 className="mb-3">Articoli</h2>
+  const addTitle = (event) => {
+    event.preventDefault(); // previene il comportamento di default del form (refresh della pagina)
+    
+    const newArticle = [...articles, newTitle]; // crea un nuovo array con il nuovo titolo
+    setArticles(newArticle); // aggiorna lo stato degli articoli
 
-        {/* Milestone 1: visualizzazione statica lista articoli */}
-        <ul className="list-group"> {/* Lista di Bootstrap */}
-          {articles.map((article, i) => (
-            <li key={i} className="list-group-item"> {/* Elemento di lista di Bootstrap */}
-              {article.title}
-            </li>
-          ))}
-        </ul>
-      </div>
+    return (
+      <> {/* React Fragment */}
+        <div className="container py-4"> {/* Bootstrap container con padding verticale */}
+          <h1 className="text-center mb-4">react-form</h1> {/* Titolo centrato con margine inferiore */}
 
-    </>
-  );
+          <h2 className="mb-3">Articoli</h2>
+
+          {/* Milestone 1: visualizzazione statica lista articoli */}
+          <ul className="list-group"> {/* Lista di Bootstrap */}
+            {articles.map((article, i) => (
+              <li key={i} className="list-group-item"> {/* Elemento di lista di Bootstrap */}
+                {article.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+      </>
+    );
+  }
 }
+
 
 export default App
 
 
 /*  Milestone 1
-
-
 Appunti sull'uso della key in React:
 
- | Sintassi        | Cosa fa                              | Quando usarla            |
+| Sintassi        | Cosa fa                              | Quando usarla            |
 | --------------- | ------------------------------------ | ------------------------ |
-| `key={a.id}`    | Usa l’id dell’oggetto (dot notation) | Migliore e più stabile |
-| `key={index}`   | Usa l’indice del ciclo               | Ok per liste statiche |
-| `key={a.title}` | Usa un altro campo univoco           | Se non si ha un id      |
-
+| `key={a.id}`    | Usa l’id dell’oggetto (dot notation) | Migliore e più stabile   |
+| `key={index}`   | Usa l’indice del ciclo               | Ok per liste statiche    |
+| `key={a.title}` | Usa un altro campo univoco           | Se non si ha un id       |
 */
