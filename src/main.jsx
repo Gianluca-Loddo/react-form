8acae2c7a2219142e1cfce8c677fe77e5bf2207b
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-10-09 18:05:01 +0200

    added import bootstrap string main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css'  // import del CSS di Bootstrap
+import 'bootstrap-icons/font/bootstrap-icons.css' // icone bootstrap
+
 import './App.css' // import mio css
 import App from './App.jsx'
 
